1d8399279faf582bdc29f5364961698b00064faf
ikspike <clifford.k.zhang@outlook.com>
2020-08-23 16:12:13 +0800

    Finish selection sort

---
+++ 02-Sortings/src/sorting/Selection_sort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Selection_sort extends General_methods
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < N; j++)
+                if (less(a[j], a[min]))
+                    min = j;
+            exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
+
+
