78dce55a5f16a20b063692c843802da446c4c79a
ikspike <clifford.k.zhang@outlook.com>
2020-11-22 21:41:32 +0800

    Update Exercise3_PriorityQueueUnorderedArray.java

--- Exercises/src/chap2/section4/Exercise3_PriorityQueueUnorderedArray.java
+++ Exercises/src/chap2/section4/Exercise3_PriorityQueueUnorderedArray.java
@@ -1,80 +1,63 @@
 package chap2.section4;
 
 @SuppressWarnings("unchecked")
-public class Exercise3_PriorityQueueUnorderedArray
-{
-    private class PriorityQueueUnorderedArray<Key extends Comparable<Key>>
-    {
+public class Exercise3_PriorityQueueUnorderedArray {
+    private class PriorityQueueUnorderedArray<Key extends Comparable<Key>> {
         private Key[] a;
         private int N = 0;
 
-        private void resize(int capacity)
-        {
+        private void resize(int capacity) {
             Key[] temp = (Key[]) new Object[capacity];
-            for (int i = 0; i < N; i++)
-            {
+            for (int i = 0; i < N; i++) {
                 temp[i] = a[i];
             }
             a = temp;
         }
 
-        private void exch(Key[] a, int m, int n)
-        {
+        private void exch(Key[] a, int m, int n) {
             Key temp = a[m];
             a[m] = a[n];
             a[n] = temp;
         }
 
-        public PriorityQueueUnorderedArray(int size)
-        {
+        public PriorityQueueUnorderedArray(int size) {
             a = (Key[]) new Comparable[size];
         }
 
-        public boolean isEmpty()
-        {
+        public boolean isEmpty() {
             return N == 0;
         }
 
-        public int size()
-        {
+        public int size() {
             return N;
         }
 
-        public void insert(Key v)
-        {
-            if (N == a.length)
-            {
+        public void insert(Key v) {
+            if (N == a.length) {
                 resize(2 * a.length);
             }
             a[N] = v;
             N++;
         }
 
-        public Key max()
-        {
+        public Key max() {
             Key max = a[0];
-            for (int i = 1; i < N; i++)
-            {
-                if (max.compareTo(a[i]) < 0)
-                {
+            for (int i = 1; i < N; i++) {
+                if (max.compareTo(a[i]) < 0) {
                     max = a[i];
                 }
             }
             return max;
         }
 
-        public Key delMax()
-        {
-            if (isEmpty())
-            {
+        public Key delMax() {
+            if (isEmpty()) {
                 throw new RuntimeException("Priority queue underflow");
             }
 
             int maxIndex = 0;
-            for (int i = 1; i < N; i++)
-            {
-                if (a[maxIndex].compareTo(a[i]) < 0)
-                {
+            for (int i = 1; i < N; i++) {
+                if (a[maxIndex].compareTo(a[i]) < 0) {
                     maxIndex = i;
                 }
             }
@@ -82,23 +65,15 @@ public class Exercise3_PriorityQueueUnorderedArray
             exch(a, maxIndex, N - 1);
             a[N - 1] = null;
             N--;
-
-            if (N > 0 && N == a.length / 4)
-            {
-                resize(a.length / 2);
-            }
-
             return maxValue;
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         testPriorityQueueUnorderedArray();
     }
 
-    public static void testPriorityQueueUnorderedArray()
-    {
+    public static void testPriorityQueueUnorderedArray() {
         PriorityQueueUnorderedArray<Integer> PQ =
                 new Exercise3_PriorityQueueUnorderedArray().new PriorityQueueUnorderedArray<>(10);
         PQ.insert(7);
